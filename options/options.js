24c93386500d9649aa76807e5800845183515b99
Crowdbotics <team@crowdbotics.com>
2021-08-30 22:09:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "dumpster_30209Identifier",
+  url: "https://dumpster_30209Identifier.botics.co",
+  api: "https://dumpster_30209Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
